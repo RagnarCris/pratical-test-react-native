eabc5d11d4fa4e3ff583a92bf256c2654bed541e
RagnarCris <cristianonascimento98@gmail.com>
2022-09-05 20:32:37 -0300

    teste hardcoded da aba de categoria e teste falho da Home

--- src/Screens/Category.js
+++ src/Screens/Category.js
@@ -1,12 +1,78 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ActivityIndicator, FlatList, SafeAreaView, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import styles from '../styles';
+import BookItem from '../Components/BookItem';
 
-const Category = ({ navigation }) => {
-  return (
-    <View>
-      <Text>Category</Text>
-    </View>
-  )
+function createRows(data, columns) {
+    const rows = Math.floor(data.length / columns);
+    let lastRowElements = data.length - rows * columns;
+  
+    while (lastRowElements !== columns) {
+      data.push({
+        id: `empty-${lastRowElements}`,
+        name: `empty-${lastRowElements}`,
+        empty: true
+      });
+      lastRowElements += 1;
+    }
+  
+    return data;
+  }
+
+const Category = ({ route, navigation }) => {
+    const list_name_encoded = route.params
+    const [carregando,setCarregando]=useState(true)
+    const [livros,setLivros]=useState([])
+    const APIKEY = "UXh26tMQFKjgsrHicJxIp1NhAAax7GaO" //Minha api-key
+    //const APIKEY = "vi0bsV0yOCA9qYnmAaOUJV4dO0BNhUGR" //api-key do projeto
+
+
+
+    useEffect(
+        ()=>{
+            fetch("https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json\?api-key\="+APIKEY)
+                .then((resp)=>resp.json())
+                .then((json) => {
+                    const resultados = json.results;
+                    setLivros(resultados.books);
+                })
+                .catch((e)=>(console.log(e)))
+                .finally(()=>setCarregando(false))
+        },[]
+    )
+    
+    return(
+         <View>
+            {
+                carregando ? 
+                <View style={{alignItems: "center", justifyContent: "center"}}>
+                <ActivityIndicator size="large" color="#8B008B" />
+                <Text> Buscando Dados </Text>
+                </View>
+                : (
+                    <SafeAreaView>
+                        <FlatList
+                        data={createRows(livros, 3)}
+                        keyExtractor={({id},index)=>index}
+                        numColumns={3}
+                        renderItem={({ item }) => {
+                            if (item.empty) {
+                            return <View style={[styles.item, styles.itemEmpty]} />;
+                            }
+                            return (
+                            <View style={styles.item}>
+                                <TouchableOpacity onPress={() => navigation.navigate('Detail')}>
+                                    <BookItem imagem={item.book_image} titulo={item.title} autor={item.author} />
+                                </TouchableOpacity>
+                            </View>
+                            );
+                        }}
+                        />
+                    </SafeAreaView>
+                )
+            }
+         </View>
+    )
 }
 
 export default Category
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,4 +1,4 @@
-import { Text, View, Image, SectionList, FlatList, TouchableOpacity } from 'react-native';
+import { Text, View, Image, SectionList, FlatList, TouchableOpacity, ActivityIndicator } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import styles from '../styles';
@@ -8,13 +8,44 @@ import { Searchbar } from 'react-native-paper';
 
 
 const Home = ({ navigation }) => {
-  const [livros, setLivros] = useState([]);
-  const [categorias, setCategorias] = useState([]);
+  const [carregando,setCarregando]=useState(true)
+  const [livros,setLivros]=useState([])
+  const [categorias,setCategorias]=useState([])
   const [pesquisa, setPesquisa] = useState('');
   const [pesquisando, setPesquisando] = useState(false);
+  
+  const APIKEY = "UXh26tMQFKjgsrHicJxIp1NhAAax7GaO" //Minha api-key
+  //const APIKEY = "vi0bsV0yOCA9qYnmAaOUJV4dO0BNhUGR" //api-key do projeto
 
-  const onChangeSearch = query => {
-    setPesquisa(query);
+  useEffect(
+      ()=>{
+          Promise.all([
+              fetch("https://api.nytimes.com/svc/books/v3/lists/names.json\?api-key\="+APIKEY),
+              fetch("https://api.nytimes.com/svc/books/v3/lists/overview.json\?api-key\="+APIKEY),
+            ])
+              .then(respostas =>
+                  Promise.all(respostas.map(resposta => resposta.json()))
+              )
+              .then( dados => {
+                  const json_categorias = dados[0];
+                  const json_completo = dados[1];
+                  setCategorias(json_categorias.results);
+                  const resultados = json_completo.results;
+                  const lista_livros = resultados.lists;
+                  const livros_aux = []
+                  // Separa 10 livros para as seções "Para você" e "Os mais lidos da semana"
+                  for (var i = 0; i < 10; i++){
+                      let item = lista_livros[i]
+                      livros_aux.push(...item.books);
+                  }
+                  setLivros(livros_aux)})
+              .catch((e)=>(console.log(e)))
+              .finally(()=>setCarregando(false))
+      },[]
+  )
+
+  const realizandoPesquisa = consulta => {
+    setPesquisa(consulta);
   };
 
   useEffect(() => {pesquisa === '' ? setPesquisando(false) : setPesquisando(true)},[pesquisa])
@@ -37,15 +68,64 @@ const Home = ({ navigation }) => {
           inputStyle={styles.searchBarInput}
           elevation='0'
           placeholder="Desabilitado"
-          onChangeText={onChangeSearch}
+          onChangeText={realizandoPesquisa}
           value={pesquisa}
         />
         </View>
-        <TouchableOpacity onPress={() => navigation.navigate('Detail')}>
-          <BookItem imagem={require("../assets/perfil.png")} titulo="Título da obra das obras" autor="Cristiano Nascimento" />
-        </TouchableOpacity>
-        <Text style={styles.sectionTitle}>Categorias</Text>
-        <CategoryItem categoria="Hardcover Political Books"/>
+        {
+        carregando ?
+        <View style={{alignItems: "center", justifyContent: "center"}}>
+        <ActivityIndicator size="large" color="#8B008B" />
+        <Text> Buscando Dados </Text>
+        </View>
+        :(
+        <SectionList
+          contentContainerStyle={{ paddingHorizontal: 10 }}
+          stickySectionHeadersEnabled={false}
+          sections={[
+          { title: 'Para Você', data: livros },
+          { title: 'Categorias', data: categorias },
+          { title: 'Os mais lidos da semana', data: livros}
+          ]}
+          renderSectionHeader={({ section }) => {
+            console.log(section.title);
+            section.title === 'Categorias' ?
+              <>
+                <Text style={styles.sectionTitle}>{section.title}</Text>
+                  <FlatList
+                      horizontal
+                      data={section.data}
+                      keyExtractor={({id},index)=>index}
+                      renderItem={({ item }) => {
+                        <TouchableOpacity onPress={() => navigation.navigate('Category', item.list_name_encoded)}>
+                          <CategoryItem categoria={item.display_name}/>
+                        </TouchableOpacity>
+                    }}
+                      showsHorizontalScrollIndicator={false}
+                  />
+              </>
+            :
+            (
+            <>
+              <Text style={styles.sectionTitle}>{section.title}</Text>
+                <FlatList
+                    horizontal
+                    data={section.data}
+                    keyExtractor={({id},index)=>index}
+                    renderItem={({ item }) => {
+                      <TouchableOpacity onPress={() => navigation.navigate('Detail')}>
+                        <BookItem imagem={item.book_image} titulo={item.title} autor={item.author} />
+                      </TouchableOpacity>
+                  }}
+                    showsHorizontalScrollIndicator={false}
+                />
+            </>
+          )}}
+          renderItem={({ item, section }) => {
+              return null;
+          }}
+        />
+        )}
     </View>
   )
 }
