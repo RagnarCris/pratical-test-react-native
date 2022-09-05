ee176ee57c69dc7b660509f9dcd25e7068dbd92c
RagnarCris <cristianonascimento98@gmail.com>
2022-09-05 19:49:11 -0300

    Teste de fetch da api e layout de SectionList para a Home

--- App.js
+++ App.js
@@ -5,16 +5,20 @@ import Category from './src/Screens/Category';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HeaderHome from './src/Components/HeaderHome';
+import FetchTest from './src/Screens/FetchTest';
+import SectionTest from './src/Screens/SectionTest';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="FetchTest">
         <Stack.Screen name="Home" component={Home} options={{headerTitle: () => (<HeaderHome/>)}} />
         <Stack.Screen name="Detail" component={Detail} options={{headerTitle: " "}} />
         <Stack.Screen name="Category" component={Category} options={{headerTitle: "Categoria"}} />
+        <Stack.Screen name="FetchTest" component={FetchTest} options={{headerTitle: " "}} />
+        <Stack.Screen name="SectionTest" component={SectionTest} options={{headerTitle: " "}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
